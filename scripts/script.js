85193ba04c2e2d23c99aa7ce377bef0818704a6a
mrosa_san <mrosa.san.mora@gmail.com>
2021-01-24 20:08:18 +0100

    subo el ejercicio

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+
+//barra de navegación desplegable
+document.querySelector(".boton-menu").addEventListener("click", ()=>{
+    document.querySelector(".navegador").classList.toggle("mostrar")
+});
+//botón de la barra desplegable
+document.querySelector(".boton-menu").addEventListener("click", ()=>{
+    document.querySelector(".boton-menu").classList.toggle("pulsado")
+});
